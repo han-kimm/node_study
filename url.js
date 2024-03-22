01c4366fc058a66a9194ba41697f9cfd8ce1a788
Haneul-Kim <4188989@naver.com>
2024-03-22 12:14:39 +0900

    test: 내장 객체 URL

---
+++ url.js
@@ -0,0 +1,9 @@
+const myUrl = new URL("https://www.naver.com/shopping?category=javascript&time=3&sale=true")
+
+console.log(myUrl.href)
+console.log(myUrl.host)
+console.log(myUrl.hostname)
+console.log(myUrl.origin)
+console.log(myUrl.pathname)
+console.log(myUrl.search)
+console.log(myUrl.searchParams)
