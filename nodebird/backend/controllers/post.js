82d55e11ec2ae572f3639939c6cffe36d45e22c1
Haneul-Kim <4188989@naver.com>
2024-03-28 09:52:12 +0900

    nodebird: 게시글 올리기

--- nodebird/backend/controllers/post.js
+++ nodebird/backend/controllers/post.js
@@ -1,11 +1,31 @@
+const Post = require('../models/post')
+const Hashtag = require('../models/hashtag')
+
 exports.afterUploadImage = (req, res) => {
   console.log(req.file);
   res.json({ url: `/img/${req.file.filename}` })
 }
 
-exports.uploadPost = async (req, rest, next) => {
+exports.uploadPost = async (req, res, next) => {
   try {
+    const post = await Post.create({
+      content: req.body.content,
+      img: req.body.url,
+      userId: req.user.id
+    });
 
+    const hashtags = req.body.content.match(/#[^\s#]*/g);
+    if (hashtags) {
+      const result = await Promise.all(hashtags.map((tag) => {
+        Hashtag.findOrCreate({
+          where: { title: tag.slice(1).toLowerCase() }
+        })
+      }))
+
+      console.log('result', result);
+      await post.addHashtags(result.map(r => r[0]));
+    }
+    res.redirect('/')
   } catch (e) {
     console.error(e)
     next(e)
