47632efc257b75edbd1f884152cf37095e191444
Haneul-Kim <4188989@naver.com>
2024-03-29 10:59:39 +0900

    jest: 미들웨어 테스트

--- nodebird/backend/middlewares/index.js
+++ nodebird/backend/middlewares/index.js
@@ -2,7 +2,7 @@ exports.isLoggedIn = (req, res, next) => {
   if (req.isAuthenticated()) {
     next();
   } else {
-    res.status(403, send({ message: "로그인 필요" }))
+    res.status(403).send({ message: "로그인 필요" })
   }
 }
 
---
+++ nodebird/backend/middlewares/index.test.js
@@ -0,0 +1,30 @@
+const { isLoggedIn, isNotLoggedIn } = require('./')
+
+describe('isLoggedIn', () => {
+  const res = {
+    status: jest.fn(() => res),
+    send: jest.fn()
+  };
+  const next = jest.fn()
+  test('로그인 o, next 호출', () => {
+    const req = {
+      isAuthenticated: jest.fn(() => true)
+    };
+    isLoggedIn(req, res, next)
+    expect(next).toBeCalledTimes(1)
+  })
+
+  test('로그인 x, 에러 응답', () => {
+    const req = {
+      isAuthenticated: jest.fn(() => false)
+    };
+    isLoggedIn(req, res, next)
+    expect(res.status).toBeCalledWith(403)
+    expect(res.send).toBeCalledWith({ message: '로그인 필요' })
+  })
+})
+describe('isNotLoggedIn', () => {
+  // test('로그인 o, 에러 응답', () => {
+  //   isNotLoggedIn()
+  // })
+})
